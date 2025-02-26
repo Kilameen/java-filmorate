6aff16eadf17e4d3680b233b53416198c25b5b25
Kilameen <kilameen@yandex.ru>
2025-02-26 21:28:58 +0300

    Выполнение задания по ТЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -1,35 +1,25 @@
 package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handlerNotFoundException(NotFoundException e) {
-        return new ErrorResponse(e.getMessage());
-    }
-
     @ExceptionHandler(ValidationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handlerValidationException(ValidationException e) {
-        return new ErrorResponse(e.getMessage());
+    public ResponseEntity<Object> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>("Ошибка валидации", HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Throwable.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handlerThrowable(Throwable e) {
-        return new ErrorResponse("Произошла непредвиденная ошибка.");
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+        return new ResponseEntity<>("Искомый объект не найден", HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationException(MethodArgumentNotValidException e) {
-        return new ErrorResponse("Ошибка валидации: " + e.getBindingResult().getFieldError().getDefaultMessage());
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("Возникло непредвиденное исключение", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
