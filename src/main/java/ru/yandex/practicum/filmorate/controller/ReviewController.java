6545953b2afbd1c51582e183fa0daf470b523b1a
Anstasia <lapteva_02@list.ru>
2025-04-24 14:47:25 +0300

    Реализованы методы контроллера и сервиса

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,96 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Marker;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.review.ReviewService;
+
+import java.util.List;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    //Добавление нового отзыва
+    @PostMapping
+    @Validated(Marker.OnCreate.class)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Review create(@Valid @RequestBody Review review) {
+        log.info("Запрос на создание отзыва {}", review);
+        return reviewService.create(review);
+    }
+
+    //Редактирование уже имеющегося отзыва
+    @PutMapping
+    @Validated(Marker.OnUpdate.class)
+    public Review update(@Valid @RequestBody Review review) {
+        log.info("Запрос на обновление отзыва {}", review);
+        return reviewService.update(review);
+    }
+
+    //Удаление уже имеющегося отзыва
+    @DeleteMapping("/{id}")
+    @Validated(Marker.OnCreate.class)
+    public void delete(@PathVariable Long id) {
+        log.info("Запрос на удаление отзыва с id {}", id);
+        reviewService.delete(id);
+    }
+
+    //Получение отзыва по идентификатору
+    @GetMapping("/{id}")
+    @Validated(Marker.OnCreate.class)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Review getReview(@PathVariable Long id) {
+        log.info("Запрос на получение отзыва с id {}", id);
+        return reviewService.getReview(id);
+    }
+
+    //Получение всех отзывов по идентификатору фильма
+    @GetMapping
+    public List<Review> getReviews(
+            @RequestParam Long filmId,
+            @RequestParam(defaultValue = "10") int count) {
+
+        //Если фильм не указан, то все
+        if(filmId==null){
+            log.info("Запрос на получение всех отзывов");
+            return reviewService.getAllReviews();
+        }
+        log.info("Запрос на получение {} отзывов фильма с id {}", count, filmId);
+        return reviewService.getReviewsByFilmId(filmId, count);
+    }
+
+    //Пользователь ставит лайк отзыву
+    @PutMapping("/{id}/like/{userId}")
+    @Validated(Marker.OnUpdate.class)
+    public void addLike(@PathVariable("id") Long reviewId, @PathVariable("userId") Long userId) {
+        log.info("Пользователь с id {} пытается поставить лайк отзыву с id {}", userId, reviewId);
+        reviewService.addLike(reviewId,userId);
+    }
+    //Пользователь ставит дизлайк отзыву
+    @PutMapping("/{id}/dislike/{userId}")
+    @Validated(Marker.OnUpdate.class)
+    public void addDislike(@PathVariable("id") Long reviewId, @PathVariable("userId") Long userId) {
+        log.info("Пользователь с id {} пытается поставить дизлайк отзыву с id {}", userId, reviewId);
+        reviewService.addDislike(reviewId,userId);
+    }
+
+    //Пользователь удаляет лайк/дизлайк отзыву
+    //DELETE /reviews/{id}/like/{userId}
+    @DeleteMapping({"/{id}/dislike/{userId}", "/{id}/like/{userId}"})
+    @Validated(Marker.OnUpdate.class)
+    public void deleteMark(@PathVariable("id") Long reviewId, @PathVariable("userId") Long userId) {
+        log.info("Пользователь с id {} пытается убрать оценку у отзыва с id {}", userId, reviewId);
+        reviewService.deleteMark(reviewId,userId);
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/service/review/ReviewServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/review/ReviewServiceImpl.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service.review;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.FilmStorage;
 import ru.yandex.practicum.filmorate.dao.review.ReviewDao;
 import ru.yandex.practicum.filmorate.dao.review.UsefulDao;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
@@ -17,9 +18,11 @@ public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewDao reviewDao;
     private final UsefulDao usefulDao;
+    private final FilmStorage filmStorage;
 
     @Override
     public Review create(Review review) {
+        filmStorage.getFilm(review.getFilm_id());
         return reviewDao.create(review);
     }
 
@@ -29,6 +32,7 @@ public class ReviewServiceImpl implements ReviewService {
         if (!reviewDao.isReviewExist(review.getId())) {
             throw new NotFoundException("Отзыв с id " + review.getId() + " не найден!");
         }
+        filmStorage.getFilm(review.getFilm_id());
         return reviewDao.update(review);
     }
 
@@ -39,13 +43,16 @@ public class ReviewServiceImpl implements ReviewService {
             throw new NotFoundException("Отзыв с id " + id + " не найден!");
         }
         usefulDao.deleteAllMarks(id);
-        log.info("Оценки для фильма с id {} удалены", id);
         reviewDao.delete(id);
         log.info("Фильм с id {} удален", id);
     }
 
     @Override
     public Review getReview(Long id) {
+        //Если отзыв не найден, то выбросит исключение
+        if (!reviewDao.isReviewExist(id)) {
+            throw new NotFoundException("Отзыв с id " + id + " не найден!");
+        }
         return reviewDao.getReviewById(id);
     }
 
@@ -56,30 +63,34 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Override
     public List<Review> getReviewsByFilmId(Long filmId, int count) {
-        return (List<Review>) reviewDao.getReviewsByFilmId(filmId,count);
+        filmStorage.getFilm(filmId);
+        return (List<Review>) reviewDao.getReviewsByFilmId(filmId, count);
     }
 
     @Override
     public void addLike(Long reviewId, Long userId) {
+        filmStorage.getFilm(reviewId);
         usefulDao.addLike(reviewId, userId);
         int likesCount = usefulDao.getLikesCountForReview(reviewId);
         int dislikesCount = usefulDao.getDislikesCountForReview(reviewId);
-        reviewDao.updateUsefulToReview(reviewId, likesCount-dislikesCount);
+        reviewDao.updateUsefulToReview(reviewId, likesCount - dislikesCount);
     }
 
     @Override
     public void addDislike(Long reviewId, Long userId) {
+        filmStorage.getFilm(reviewId);
         usefulDao.addDislike(reviewId, userId);
         int likesCount = usefulDao.getLikesCountForReview(reviewId);
         int dislikesCount = usefulDao.getDislikesCountForReview(reviewId);
-        reviewDao.updateUsefulToReview(reviewId, likesCount-dislikesCount);
+        reviewDao.updateUsefulToReview(reviewId, likesCount - dislikesCount);
     }
 
     @Override
     public void deleteMark(Long reviewId, Long userId) {
+        filmStorage.getFilm(reviewId);
         usefulDao.deleteMark(reviewId, userId);
         int likesCount = usefulDao.getLikesCountForReview(reviewId);
         int dislikesCount = usefulDao.getDislikesCountForReview(reviewId);
-        reviewDao.updateUsefulToReview(reviewId, likesCount-dislikesCount);
+        reviewDao.updateUsefulToReview(reviewId, likesCount - dislikesCount);
     }
 }
