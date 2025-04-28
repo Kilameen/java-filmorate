5ac4f1c98127990034eb7f7d9f026eb9d6068430
Kilameen <Kilameen@yandex.ru>
2025-04-28 13:17:41 +0300

    Добавлена функция "Лента событий"

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -64,7 +64,7 @@ public class FilmDbStorage implements FilmStorage {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
             PreparedStatement preparedStatement = connection
-                    .prepareStatement(INSERT_FILM_SQL ,
+                    .prepareStatement(INSERT_FILM_SQL,
                             Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, film.getName());
             preparedStatement.setString(2, film.getDescription());
